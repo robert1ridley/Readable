df2a4dfc26f880e98227a0db17dd8aedaf400082
Robert Ridley <robert.n.ridley@gmail.com>
2018-03-06 22:02:42 +0800

    tidy up routes file

--- frontend/src/routes/Routes.js
+++ frontend/src/routes/Routes.js
@@ -6,29 +6,15 @@ import NotFound from '../pages/NotFound';
 import Header from '../components/Header';
 
 class Routes extends React.Component {
-  state = {
-    posts: null
-  }
-
-  // componentDidMount() {
-  //   fetch(`http://localhost:3001/posts`, { headers: { 'Authorization': Math.random().toString(36).substr(-8) }})
-  //     .then(res => res.json())
-  //     .then(data => 
-  //       this.setState({
-  //         posts: data
-  //       })
-  //     )
-  // }
 
   render() {
-    const { posts } = this.state;
     return (
       <BrowserRouter>
         <div>
           <Header />
           <Switch>
-            <Route exact path="/" render={() => <Home posts={posts}/>}/>
-            <Route path="/:id" render={() => <SingleCategory posts={posts}/>}/>
+            <Route exact path="/" component={Home} />
+            <Route path="/:id" component={SingleCategory} />
             <Route path="*" component={NotFound} />
           </Switch>
         </div>
