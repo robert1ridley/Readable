b623be7227ca22235725fcd7c6be5d98f7930dfc
Robert Ridley <robert.n.ridley@gmail.com>
2018-03-07 22:29:01 +0800

    add icons to posts view

--- frontend/src/components/PostsList.js
+++ frontend/src/components/PostsList.js
@@ -4,12 +4,18 @@ import moment from 'moment';
 import List, { ListItem, ListItemText } from 'material-ui/List';
 import Avatar from 'material-ui/Avatar';
 import FaceIcon from 'material-ui-icons/Face';
+import BottomNavigation, { BottomNavigationAction } from 'material-ui/BottomNavigation';
+import ThumbUp from 'material-ui-icons/ThumbUp';
+import ThumbDown from 'material-ui-icons/ThumbDown';
+import DateRangeIcon from 'material-ui-icons/DateRange';
+import LocationOnIcon from 'material-ui-icons/LocationOn';
+import InsertCommentIcon from 'material-ui-icons/InsertComment';
 
 const styles = theme => ({
   root: {
     width: '100%',
     backgroundColor: theme.palette.background.paper,
-    marginTop: 30
+    marginTop: 30,
   },
 });
 
@@ -20,19 +26,22 @@ function PostsList(props) {
       <List>
         {
           posts.map(post => 
-            <div key={post.id}>
+            <div key={post.id} style={{marginBottom: 30, cursor: 'pointer'}}>
               <ListItem>
                 <Avatar>
                   <FaceIcon />
                 </Avatar>
                 <ListItemText primary={post.title} secondary={post.body} />
               </ListItem>
-              <p style={{marginLeft:70, fontSize: '80%', fontWeight: 500}}>Likes: 
-                <span style={{fontWeight: 100}}> {post.voteScore} </span>
-                Date:
-                <span style={{fontWeight: 100}}> {`${new Date(post.timestamp).getDay()}/${new Date(post.timestamp).getMonth()}/${new Date(post.timestamp).getFullYear()}`}</span>
-                {console.log(new Date(post.timestamp))}
-              </p>
+              <BottomNavigation
+                value='blue'
+                showLabels
+                style={{maxWidth: 400}}
+              >
+                <BottomNavigationAction label={post.voteScore} icon={post.voteScore < 0 ? <ThumbDown /> : <ThumbUp />} />
+                <BottomNavigationAction label={`${new Date(post.timestamp).getDay()}/${new Date(post.timestamp).getMonth()}/${new Date(post.timestamp).getFullYear()}`} icon={<DateRangeIcon />} />
+                <BottomNavigationAction label={`${post.commentCount} comments`} icon={<InsertCommentIcon />} />
+              </BottomNavigation>
             </div>
           )
         }
