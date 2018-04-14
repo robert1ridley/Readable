8d4e1b7fd71fe7b1f6ae4384fb2e29a73b56c2f8
Robert Ridley <robert.n.ridley@gmail.com>
2018-04-14 12:03:37 +0800

    render not-found page on invalid url path for single-post page

--- frontend/src/pages/SingleCategory.js
+++ frontend/src/pages/SingleCategory.js
@@ -3,36 +3,74 @@ import { connect } from 'react-redux';
 import PostsList from '../components/PostsList';
 import { fetchPostsByCategory } from '../Actions/postsActions';
 import SortButtons from '../components/SortButtons';
+import NotFound from './NotFound';
 
 class SingleCategory extends React.Component {
+  state = {
+    notFound: false
+  }
+
   componentDidMount() {
-    this.props.dispatch(fetchPostsByCategory(this.props.match.params.category));
+    this.props.dispatch(fetchPostsByCategory(this.props.match.params.category))
+    .then(() => {
+      const pageId = this.props.match.params.category;
+      const foundIndex = this.props.categories.findIndex((el) => (el.name === pageId));
+      console.log(foundIndex)
+      foundIndex<0 ?
+        this.setState ({
+          notFound: true
+        }):
+        this.setState ({
+          notFound: false
+        })
+      }
+    )
   }
 
   componentWillReceiveProps(nextProps) {
     if (nextProps.match.params.category !== this.props.match.params.category && !this.props.loading){
-      this.props.dispatch(fetchPostsByCategory(nextProps.match.params.category));
+      this.props.dispatch(fetchPostsByCategory(nextProps.match.params.category))
+      .then(() => {
+        const pageId = nextProps.match.params.category;
+        const foundIndex = nextProps.categories.findIndex((el) => (el.name === pageId));
+        console.log(foundIndex)
+        foundIndex<0 ?
+          this.setState ({
+            notFound: true
+          }):
+          this.setState ({
+            notFound: false
+          })
+        }
+      )
     }
   }
 
   render(){
-    let { posts } = this.props
-    return (
-      <div>
-        <SortButtons />
-        {
-          posts &&
-          <PostsList posts={posts} />
-        }
-      </div>
-    )
+    const { posts } = this.props
+    const { notFound } = this.state
+    if(notFound){
+      return(<NotFound/>)
+    }
+    else{
+      return (
+        <div>
+          <SortButtons />
+          {
+            posts &&
+            <PostsList posts={posts} />
+          }
+        </div>
+      )
+    }
   }
 }
 
 const mapStateToProps = state => ({
   posts: state.postsReducer.items,
   loading: state.postsReducer.loading,
-  error: state.postsReducer.error
+  error: state.postsReducer.error,
+  categories: state.categoryReducer.items
 });
 
 export default connect(mapStateToProps)(SingleCategory);
